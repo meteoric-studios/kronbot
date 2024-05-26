503d27647d6d60952ee3931688f53a9bd3036b1b
in-th3-l00p <indianucode0303@gmail.com>
2024-05-26 21:17:53 +0300

    separate icon file

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,6 +4,7 @@ import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 import {useEffect, useRef, useState} from "react";
+import {ContactIcon, SidebarIcon} from "@/components/icons";
 
 function Branding() {
     return (
@@ -124,9 +125,7 @@ function Nav() {
                         "hover:bg-gray transition-all"
                     )}
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
-                    </svg>
+                    <SidebarIcon />
                 </button>
                 {sidebarOpen && (
                     <Sidebar setSidebarOpen={setSidebarOpen} />
@@ -147,9 +146,7 @@ function Nav() {
 
             <NavLink href={"/"}>
                 Contact
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
-                </svg>
+                <ContactIcon />
             </NavLink>
         </>
     );
